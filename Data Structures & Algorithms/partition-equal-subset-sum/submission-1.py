347f658bf1251171b2e7835c634671ca4212ef2f
anuritaG <58184024+anuritaG@users.noreply.github.com>
2026-04-25 17:47:04 +0530

    Add: partition-equal-subset-sum - submission-1

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        # DP solution is to check which numbers can be joined to form the target
+        # dp[i][j] can the first i no.s form a sum of j
+        # at any (i,j) we have 2 options: skip num(i) dp(i,j) = dp(i-1,j)
+        # or if (j-i) >= 0 then select i, dp(i,j) = dp(i-1, j -nums(i)) 
+        # basically, if we take i it becomes true only if the first (i-1) no. can sum to (j-nums(i)) target
+        total = sum(nums)
+        if total % 2 != 0:
+            return False
+        target = total // 2
+        n = len(nums)
+        dp=[[False] * (target+1) for _ in range(n+1)]
+        # j = 0, we can always make 0 if we do not take any element
+        for i in range(n+1):
+            dp[i][0] = True
+        for i in range(1,n+1):
+            for j in range(1, target+1):
+                dp[i][j] = dp[i-1][j]
+                if nums[i-1] <= j:
+                    dp[i][j] = dp[i][j] or dp[i-1][j-nums[i-1]]
+        return dp[n][target]
+ 
+        
