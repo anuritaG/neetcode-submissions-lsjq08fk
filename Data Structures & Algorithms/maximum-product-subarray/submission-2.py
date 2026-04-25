1ede60575b1ff1ea447c1daa919fbc7d0a4aa570
anuritaG <58184024+anuritaG@users.noreply.github.com>
2026-04-25 10:23:15 +0530

    Add: maximum-product-subarray - submission-2

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        # If we take the abs value of each number, if we iterate through each
+        # number and multiply it, the value is always non-decreasing.
+        # For the sign, we also need to store the min. value because multiplying it
+        # with another - sign can make it max. as well
+        curMax, curMin = 1, 1
+        maxVal = nums[0]
+        for num in nums:
+            tmp = curMax
+            curMax = max(curMax*num, num, curMin*num)
+            curMin = min(tmp*num, num, curMin*num)
+            maxVal = max(maxVal, curMax)
+            # print(num, " ", curMax, " ", curMin)
+        return maxVal
+        
