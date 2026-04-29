e6e4f40248aded444490f47c73d1f51a91045e7b
anuritaG <58184024+anuritaG@users.noreply.github.com>
2026-04-29 19:40:57 +0530

    Add: meeting-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-1.py
@@ -0,0 +1,24 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    # Sweep Line algo:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        intervals.sort(key=lambda x:x.start)
+        mp = defaultdict(int)
+        for interval in intervals:
+            mp[interval.start] += 1
+            mp[interval.end] -= 1
+        res = 0
+        rooms = 0
+        for i in sorted(mp):
+            rooms += mp[i]
+            res = max(res, rooms)
+        return res
+
+        
