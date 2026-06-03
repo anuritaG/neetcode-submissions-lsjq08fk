bf22bedd98ddcb50656a890738f2742519fa19dc
anuritaG <58184024+anuritaG@users.noreply.github.com>
2026-06-03 19:31:01 +0530

    Add: rotting-fruit - submission-2

---
+++ Data Structures & Algorithms/rotting-fruit/submission-2.py
@@ -0,0 +1,45 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        # multi source BFS,
+        nrows = len(grid)
+        ncols = len(grid[0])
+        visit = set()
+        q = deque()
+        fresh = 0
+        def addCell(r, c):
+            # decrease the counter for fresh while adding to queue not while popping
+            # during addition it is already rotten
+            nonlocal fresh
+            if r < 0 or r >= nrows or c < 0 or c >= ncols or (r,c) in visit:
+                return
+            if grid[r][c] != 1:
+                return
+            q.append([r,c])
+            visit.add((r,c))
+            fresh -= 1
+        for r in range(nrows):
+            for c in range(ncols):
+                if grid[r][c] == 2:
+                    q.append([r,c])
+                    visit.add((r,c))
+                if grid[r][c] == 1:
+                    fresh += 1
+                 
+        time = 0
+        while fresh>0 and q:
+            length = len(q)
+            for i in range(length):
+                node = q.popleft()
+                r = node[0]
+                c = node[1]
+                addCell(r-1, c)
+                addCell(r+1, c)
+                addCell(r, c-1)
+                addCell(r, c+1)
+            time += 1
+        print(time)
+        if fresh > 0:
+            return -1
+        return time
+
+            
