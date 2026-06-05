12f9d802dadca6d8a34f22f6a6925ccb8c710db7
anuritaG <58184024+anuritaG@users.noreply.github.com>
2026-06-05 19:45:00 +0530

    Add: count-connected-components - submission-0

---
+++ Data Structures & Algorithms/count-connected-components/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        res = 0
+        q = deque()
+        visit = set()
+        adj = [set() for _ in range(n)]
+        for edge in edges:
+            print(edge)
+            print(edge[0])
+        
+            adj[edge[0]].add(edge[1])
+            adj[edge[1]].add(edge[0])
+        def bfs():
+            nonlocal q
+            nonlocal visit
+            while q:
+                node = q.popleft()
+                for neigh in adj[node]:
+                    if neigh not in visit:
+                        visit.add(neigh)
+                        q.append(neigh)
+
+        for node in range(n):
+            if node not in visit:
+                res += 1
+                q.append(node)
+                visit.add(node)
+                bfs()
+        return res
